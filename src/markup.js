71426cf46eed6dbf97ff962a42393885aa5b4c53
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2025-04-07 21:07:25 +0530

    Add menu markup

--- src/markup.js
+++ src/markup.js
@@ -9,7 +9,7 @@ headDiv.classList.add('head-div')
 //restaurant-name
 const restaurantName = document.createElement('p')
 restaurantName.classList.add('restaurant-name')
-restaurantName.textContent = 'Bagara'
+restaurantName.textContent = 'Restaurant'
 headDiv.appendChild(restaurantName)
 //navigation
 const nav = document.createElement('nav')
@@ -42,6 +42,7 @@ para.textContent = `Enjoy a South Indian feast with crispy dosas, fluffy idlis, 
 export const menuDescription = document.createElement('div')
 menuDescription.classList.add('menu-description')
 informationDiv.appendChild(menuDescription)
+//item-dosa
 let div = document.createElement('div')
 div.classList.add('dosa-info')
 let img = document.createElement('img')
@@ -51,6 +52,29 @@ img.setAttribute('height', '180')
 img.setAttribute('width', '180')
 div.appendChild(img)
 menuDescription.appendChild(div)
+let outerDiv = document.createElement('div')
+let innerDiv = document.createElement('div')
+innerDiv.classList.add('item-name')
+para = document.createElement('p')
+para.textContent = 'ITEM'
+para.classList.add('item')
+innerDiv.appendChild(para)
+para  = document.createElement('p')
+para.textContent = 'DOSA'
+innerDiv.appendChild(para)
+outerDiv.appendChild(innerDiv)
+innerDiv = document.createElement('div')
+innerDiv.classList.add('item-price')
+para = document.createElement('p')
+para.textContent = 'PRICE'
+para.classList.add('price')
+innerDiv.appendChild(para)
+para  = document.createElement('p')
+para.textContent = '100 INR'
+innerDiv.appendChild(para)
+outerDiv.appendChild(innerDiv)
+div.appendChild(outerDiv)
+//item-chicken
 div = document.createElement('div')
 div.classList.add('chicken-info')
 img = document.createElement('img')
@@ -60,6 +84,29 @@ img.setAttribute('height', '200')
 img.setAttribute('width', '240')
 div.appendChild(img)
 menuDescription.appendChild(div)
+outerDiv = document.createElement('div')
+innerDiv = document.createElement('div')
+innerDiv.classList.add('item-name')
+para = document.createElement('p')
+para.textContent = 'ITEM'
+para.classList.add('item')
+innerDiv.appendChild(para)
+para  = document.createElement('p')
+para.textContent = 'CHICKEN'
+innerDiv.appendChild(para)
+outerDiv.appendChild(innerDiv)
+innerDiv = document.createElement('div')
+innerDiv.classList.add('item-price')
+para = document.createElement('p')
+para.textContent = 'PRICE'
+para.classList.add('price')
+innerDiv.appendChild(para)
+para  = document.createElement('p')
+para.textContent = '100 INR'
+innerDiv.appendChild(para)
+outerDiv.appendChild(innerDiv)
+div.appendChild(outerDiv)
+//item-idli
 div = document.createElement('div')
 div.classList.add('idli-info')
 img = document.createElement('img')
@@ -69,6 +116,29 @@ img.setAttribute('height', '180')
 img.setAttribute('width', '180')
 div.appendChild(img)
 menuDescription.appendChild(div)
+outerDiv = document.createElement('div')
+innerDiv = document.createElement('div')
+innerDiv.classList.add('item-name')
+para = document.createElement('p')
+para.textContent = 'ITEM'
+para.classList.add('item')
+innerDiv.appendChild(para)
+para  = document.createElement('p')
+para.textContent = 'IDLI'
+innerDiv.appendChild(para)
+outerDiv.appendChild(innerDiv)
+innerDiv = document.createElement('div')
+innerDiv.classList.add('item-price')
+para = document.createElement('p')
+para.textContent = 'PRICE'
+para.classList.add('price')
+innerDiv.appendChild(para)
+para  = document.createElement('p')
+para.textContent = '100 INR'
+innerDiv.appendChild(para)
+outerDiv.appendChild(innerDiv)
+div.appendChild(outerDiv)
+//item-pongal
 div = document.createElement('div')
 div.classList.add('pongal-info')
 img = document.createElement('img')
@@ -78,7 +148,29 @@ img.setAttribute('height', '180')
 img.setAttribute('width', '180')
 div.appendChild(img)
 menuDescription.appendChild(div)
-//about-descriptin 
+outerDiv = document.createElement('div')
+innerDiv = document.createElement('div')
+innerDiv.classList.add('item-name')
+para = document.createElement('p')
+para.textContent = 'ITEM'
+para.classList.add('item')
+innerDiv.appendChild(para)
+para  = document.createElement('p')
+para.textContent = 'PONGAL'
+innerDiv.appendChild(para)
+outerDiv.appendChild(innerDiv)
+innerDiv = document.createElement('div')
+innerDiv.classList.add('item-price')
+para = document.createElement('p')
+para.textContent = 'PRICE'
+para.classList.add('price')
+innerDiv.appendChild(para)
+para  = document.createElement('p')
+para.textContent = '100 INR'
+innerDiv.appendChild(para)
+outerDiv.appendChild(innerDiv)
+div.appendChild(outerDiv)
+//about-description 
 export const aboutDescription = document.createElement('div')
 aboutDescription.classList.add('about-description')
 informationDiv.appendChild(aboutDescription)
